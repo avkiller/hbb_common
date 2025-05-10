bedd75be857efa87f2248066c68c487c424c6ea9
fireworld <fuxudong103@gmail.com>
2025-05-10 10:50:32 +0800

    同步代码

--- src/config.rs
+++ src/config.rs
@@ -1915,13 +1915,13 @@ impl UserDefaultConfig {
             keys::OPTION_IMAGE_QUALITY => {
                 self.get_string(key, "low", vec!["best", "low", "custom", "balanced"])
             }
-            keys:: OPTION_CODEC_PREFERENCE => {
+            keys::OPTION_CODEC_PREFERENCE => {
                 self.get_string(key, "auto", vec!["vp8", "vp9", "av1", "h264", "h265"])
             }
             keys::OPTION_CUSTOM_IMAGE_QUALITY => self.get_num_string(key, 50.0, 10.0, 0xFFF as f64),
             keys::OPTION_CUSTOM_FPS => self.get_num_string(key, 30.0, 5.0, 120.0),
             keys::OPTION_ENABLE_FILE_COPY_PASTE => self.get_string(key, "Y", vec!["", "N"]),
-	    keys::OPTION_TRACKPAD_SPEED => self.get_num_string(key, 100, 10, 1000),
+            keys::OPTION_TRACKPAD_SPEED => self.get_num_string(key, 100, 10, 1000),
             // add some option by fireworld
             keys::OPTION_DISABLE_AUDIO => self.get_string(key, "Y", vec!["", "N"]),
             keys::OPTION_SHOW_QUALITY_MONITOR => self.get_string(key, "Y", vec!["", "N"]),
